10142b15878e61bda592508d56c1985a471731d1
Ashika007 <balamiashika007@gmail.com>
2024-10-17 16:18:58 +0545

    commit instruction added

---
+++ index.js
@@ -0,0 +1,6 @@
+//initialize that it is a git project
+//git remote add origin https://github.com/Ashika007/web.git - code ka push agreko
+//remote add origin line
+//git commit -m " files added"
+
+// steps: add - commit - push
